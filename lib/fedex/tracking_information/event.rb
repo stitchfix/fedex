121ad219b554b48e1f665932e0bff059fdc2a859
Stephen Howell <stephen.howell@stitchfix.com>
2020-04-16 16:18:33 -0700

    Use #dig instead of #[] for obtaining address info
    In a tracking event, it is not guaranteed that an address will exist in
    the event details.  This is not clear in the Fedex documentation
    (https://www.fedex.com/us/developer/downloads/pdf/2019/FedEx_WebServices_DevelopersGuide_v2019.pdf)
    but in the WSDL
    (https://www.fedex.com/us/developer/downloads/wsdl/2019/standard/TrackService.zip)
    the spec is as follows:
    
    ```xml
    <xs:complexType name="TrackEvent">
      <xs:annotation>
        <xs:documentation>FedEx scanning information about a package.</xs:documentation>
      </xs:annotation>
      <xs:sequence>
        <xs:element name="Address" type="ns:Address" minOccurs="0">
          <xs:annotation>
            <xs:documentation>Address information of the station that is responsible for the scan.</xs:documentation>
          </xs:annotation>
        </xs:element>
    ```
    
    Therefore, the code must be able to handle the case where no Address
    block exists.  This change extracts values with #dig which will return
    `nil` if any intermediate step returns `nil` from a Hash/Array.  It
    could be asked if there are any repercussions for returning nil values.
    There is precedent for events to have an Address block but not contain
    any address info.  For example, the "Shipment information sent to Fedex"
    event contains an Address block but city, state/province, postal code,
    etc. all do not exist and would be set to `nil`.

--- lib/fedex/tracking_information/event.rb
+++ lib/fedex/tracking_information/event.rb
@@ -8,11 +8,16 @@ module Fedex
         @description           = details[:event_description]
         @type                  = details[:event_type]
         @occured_at            = Time.parse(details[:timestamp])
-        @city                  = details[:address][:city]
-        @state                 = details[:address][:state_or_province_code]
-        @postal_code           = details[:address][:postal_code]
-        @country               = details[:address][:country_code]
-        @residential           = details[:address][:residential] == "true"
+        # Address may not exist, so we use #dig as it will extract a deeply-nested key or return nil if any intermediate
+        # step is nil. There are cases when :address is not nil but city or state/province code is nil (like a "shipping
+        # details sent" event) so there is precedent for some of these fields to be nil.  See the WSDL provided by
+        # Fedex for more information: https://www.fedex.com/us/developer/downloads/wsdl/2019/standard/TrackService.zip
+        # (via: https://www.fedex.com/en-us/developer/web-services/process.html#documentation)
+        @city                  = details.dig(:address, :city)
+        @state                 = details.dig(:address, :state_or_province_code)
+        @postal_code           = details.dig(:address, :postal_code)
+        @country               = details.dig(:address, :country_code)
+        @residential           = details.dig(:address, :residential) == "true"
         @exception_code        = details[:status_exception_code]
         @exception_description = details[:status_exception_description]
       end
